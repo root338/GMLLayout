0bf26cd1cd2598b7e609649e5ed3b4acf62c2802
ym <ym@yuemei.com>
2020-04-10 15:50:51 +0800

    创建demo项目

---
+++ GMLLayout/GMLViewLayoutAttributes.swift
@@ -0,0 +1,13 @@
+//
+//  GMLViewLayoutAttributes.swift
+//  GMLLayoutDemo
+//
+//  Created by apple on 2020/4/10.
+//  Copyright © 2020 GML. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+}
---
+++ GMLLayoutDemo/GMLLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GMLLayoutDemo
+//
+//  Created by apple on 2020/4/10.
+//  Copyright © 2020 GML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
